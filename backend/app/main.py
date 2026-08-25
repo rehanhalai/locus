840ed50ec09e108983698c6173dae365d42531c0
Rehan Halai <halairehan01@gmail.com>
2026-08-25 15:36:38 +0530

    initialized a fastapi backend server with /health endpoint

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Locus API",
+    description="Locus APIMulti-Vendor DVR/NVR Forensic Analysis & Recovery Tool",
+    version="0.1.0"
+)
+
+@app.get("/health")
+def health_check():
+    return {
+        "status" : "online"
+    }
